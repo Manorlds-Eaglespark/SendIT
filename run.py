349b5e7e8695819222944b580899f04ce29d69e0
Anorld Mukone <manorldsapiens@gmail.com>
2018-11-22 14:33:24 +0300

    added admin

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import os
 from app import create_app
+from app.models.Admin import Admin
+from app.data_store.data import admin_data
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
@@ -8,6 +10,9 @@ from app.database.Database import Database
 
 database = Database()
 database.create_all_tables()
+database.delete_all_tables()
+admin = Admin(admin_data)
+database.save_new_user(admin)
 
 if __name__ == '__main__':  
     app.run()
